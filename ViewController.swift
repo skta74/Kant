5858b388123bd9fe66f4aa2493e8b9fee53224ec
skta74 <keita.satooo@gmail.com>
2016-11-30 00:44:11 +0900

    xcodeテスト
    xcode管理下のコードをコミットしてみる

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 佐藤 慧太 on 2016/11/30.
+//  Copyright (c) 2016年 佐藤 慧太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
